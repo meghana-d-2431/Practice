75127e1a74ab5fdd7e5e787793894d97b2cecf8c
Megh <meghana@caredocs.ai>
2026-04-12 17:56:07 -0500

    Car

---
+++ Java/src/sample/Car.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Car {
+    // 1. fields
+    String brand;
+    int year;
+
+    // 2. Constructor
+    public Car(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    // 3. Method
+    public void displayInfo() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+
+    // Main     
+     public static void main(String[] args) {
+        // Create an "Object" (Instance of the class)
+        Car myCar = new Car("Toyota", 2024);
+        
+        // Access attributes and call methods
+        myCar.displayInfo();
+    }
+}
